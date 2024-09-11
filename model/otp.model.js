7d1d5e3c0969672f167c35a4ad92642c4082f182
prince-bodar <bodarprins4@gmail.com>
2024-09-11 11:43:03 +0530

    11-09-2024

--- model/otp.model.js
+++ model/otp.model.js
@@ -3,16 +3,20 @@ const mongoose = require('mongoose');
 const otpSchema  = mongoose.Schema({
     email: {
         type: String,
-        required:true,
+        required: true,
     },
     otp: {
         type: Number,
-        required:true,
+        required: true,
     },
-},{
-    versionkey:false,
-    timestamp:true
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 60, // OTP expires after 60 seconds (1 minute)
+    }
+}, {
+    versionKey: false,
+    timestamps: true,
 });
-
 
 module.exports = mongoose.model("OTP", otpSchema);
